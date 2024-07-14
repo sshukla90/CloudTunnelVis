1a8fce4fae7b90b14923a373309d100b6fe2a3cd
sshukla90 <sshukla90@outlook.com>
2024-07-14 16:59:30 +0000

    tabulate dashboard

--- scripts/tunnel-dashboard-runner.py
+++ scripts/tunnel-dashboard-runner.py
@@ -3,6 +3,7 @@ from nornir import InitNornir
 from nornir_utils.plugins.functions import print_result
 from nornir_scrapli.tasks import send_command
 from rich import print as rprint
+from tabulate import tabulate
 
 
 nr = InitNornir(config_file="../nornir_data/config.yaml")
@@ -10,15 +11,25 @@ nr = InitNornir(config_file="../nornir_data/config.yaml")
 def get_tunnel_info(task):
     tunnel_info= task.run(task=send_command, command="sh crypto session detail")
     structurd_output = tunnel_info.scrapli_response.textfsm_parse_output()
-    rprint(structurd_output)
+    #rprint(structurd_output)
 
-def get_interface_status():
-    pass
+    table_data = []
 
-def get_bgp_info():
-    pass
+    for item in structurd_output:
+        table_data.append([
+            task.host,             # Hostname
+            item.get('interface', ''),
+            item.get('session_status', ''),
+            item.get('uptime', ''),
+            item.get('peer', ''),
+            item.get('ikev1_status', ''),
+            item.get('remote_ip', '')
+        ])
+    headers = ["Host", "Interface", "Session Status", "Uptime", "Peer", "IKEv1 Status", "Remote IP"]
+    rprint(tabulate(table_data, headers=headers, tablefmt="pretty"))
+
 result = nr.run(task=get_tunnel_info)
-print_result(result)
+#print_result(result)
 
 
 
